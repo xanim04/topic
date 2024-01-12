847df54b58cc9065be799b44c2af084568baa469
xanim04 <velizadexanim62@gmail.com>
2024-01-12 17:39:09 +0400

    changed

---
+++ task7/main.js
@@ -0,0 +1,37 @@
+//task1
+//alert("My name is Murad");
+
+//task2
+//const userName = 'Xanim';
+//console.log(userName);
+
+//task3
+//person 
+//$add
+
+//task4
+//let bookPrice = 750;
+//let bookAmount = 14;
+//let totalPrice = bookPrice * bookAmount;
+//console.log(totalPrice);
+
+//task5
+//let kmhSpeed = 75;
+//let msSpeed = kmhSpeed * 10 / 36;
+//console.log(msSpeed);
+
+//task6
+//let year = prompt("Dogum ilini daxil et");
+//const date = new Date()
+//let nowYear = date.getFullYear();
+//let age = nowYear - year;
+//console.log("Bu gune qeder yashadiginiz gunlerin sayi: " + age * 365);
+
+//task7
+
+
+
+//task8
+//let firstName = 'Alan';
+//let lastName = 'Turing';
+// console.log(`Sizin adini z ${firstName} ${lastName}. Tanishligimiza çox şadam!`);
