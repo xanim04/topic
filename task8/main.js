cc65bff5f95f5ad3a6c99a4dedee9dd9ea8b53d4
xanim04 <velizadexanim62@gmail.com>
2024-02-02 16:33:54 +0400

    changed

--- task8/main.js
+++ task8/main.js
@@ -1,5 +1,73 @@
-let i = +prompt()
-while (i > 0) {
-    console.log("I know how to use circles", i);
-    i--
+//task1
+// function name(username) {
+//     alert(`${username} is active now`)
+// }
+// name("Xanim")
+// name("Sevinc")
+// name("Keysa")
+
+//task2
+// function compareNum(a, b, c) {
+//     if (a > b && a > c) {
+//         console.log(a);
+//     }else if (b > a && b > c) {
+//         console.log(b);
+//     }else if (c > a && c > b) {
+//         console.log(c);
+//     }else {
+//         console.log("Yalnis deyer");
+//     }
+// }
+// compareNum(4, 5, 6)
+// compareNum(7, 9, 5)
+// compareNum(3, 6, 8)
+
+
+//task3
+// function showBalance(rubl, usd) {
+//     let result = rubl + (usd * 75)
+//     console.log(result);
+// }
+// showBalance(1200, 20)
+
+//task4
+function numberToText(number) {
+    let birlikler = ["bir", "iki", "uc", "dord", "bes", "alti", "yeddi", "sekkiz", "doqquz"];
+    let onluqlar = ["on", "iyirmi", "otuz", "qırx", "əlli", "altmış", "yetmiş", "səksən", "doxsan"];
 }
+
+
+//task5
+// function compareNum(a, b) {
+//     if (a > b) {
+//         console.log(b);
+//     }
+//     else if (b > a){
+//         console.log(a);
+//     }else {
+//          console.log("Yalnis deyer");
+//        }
+// }
+// compareNum(9, 5)
+
+
+//task6
+// function calc(a, b, c) {
+//     let result;
+//     switch (c) {
+//         case "+":
+//             result = a + b
+//             break;
+//         case "-":
+//             result = a - b
+//             break;
+//         case "/":
+//             result = a / b
+//             break;
+//          case "*":
+//             result = a * b
+//             break;
+//     }
+//     console.log(result);
+// }
+// calc(4, 6, "+")
