f87a2efd1002260aad0d67f01560d6f3c18af70d
xanim04 <velizadexanim62@gmail.com>
2024-02-16 19:09:56 +0400

    added

---
+++ task12/main.js
@@ -0,0 +1,90 @@
+// Task01
+// let users = ["Sevinc","Xanim", "Ibrahim"]
+// users.push("Huseyn")
+// users.shift()
+// console.log(users);
+
+
+//task2
+// let numArray = [4, 7, 8, 3, 5, 2, 9, 4, 1, 6, 8]
+// let maxArray = Mathmax(...numArray);
+// console.log(maxArray);
+
+
+// Task03
+// let employees = [
+//   [ 'Jaylee Macy', 'marketing' ],
+//   [ 'John Smith', 'management' ],
+//   [ 'Blossom Hartley', 'design' ],
+//   [ 'Austin Carpenter', 'marketing' ],
+//   [ 'Joan Knowles', 'development' ],
+//   [ 'Sally Nunez', 'management' ],
+//   [ 'Laurel Ward', 'development' ],
+//   [ 'Lark Simon', 'marketing' ],
+//   [ 'Jane Stone', 'management' ],
+//   [ 'Courtney Olson', 'development' ],
+// ];
+// let development = [];
+// employees.forEach(name);
+// function name(item) {
+//     if (item[1] === "development") {
+//     development.push(item[0])
+// }
+
+// }
+// console.log(development);
+
+
+//task5
+// let number = [2, -5, 8, -9, 4]
+// let result = number.filter(item => item < 0)
+// console.log(result);
+
+
+//task6
+// let number = [2, -5, 8, -9, 4]
+// let result = number.filter(item => item > 0)
+// console.log(result);
+
+
+//task7
+// const names = ["Michael", "Trevor", "Franklin", "Lamar", "Jimmy"];
+// function printWithDashes(name) {
+//     console.log("--------");
+//     console.log(name);
+//     console.log("---------");
+// }
+// names.forEach(printWithDashes);
+// function printWithHearts(name) {
+//     console.log("<3<3<3<3<3" + name + "<3<3<3<3<3");
+// }
+// names.forEach(printWithHearts);
+// function printWithIndex(name, index) {
+//     console.log(index + "-" + name);
+// }
+// names.forEach(printWithIndex);
+
+//task9
+// let numbers = [1, 5, -7, 3, -9, 4, -6, 2];
+// function modifyArray(arr) {
+//     return arr.map(num => {
+//         if (num < 0) {
+//             return Math.abs(num);
+//         } else {
+//             return num * 2;
+//         }
+//     });
+// }
+// let modifiedNumbers = modifyArray(numbers);
+// console.log(modifiedNumbers);
+
+
+//task10
+// let text = ["Salam"]
+// let result = text.map(item => item.substring(0, 3))
+// console.log(result);
+
+//task11
+// let name = ["Sevinc", "Ayten", "Anar", "Ruslan"]
+// let result = name.filter(item => item[0] === "A")
+// console.log(result);
