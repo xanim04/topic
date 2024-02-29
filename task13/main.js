d83844d1dc19a4d85f58040922b6d2e62d33b198
xanim04 <velizadexanim62@gmail.com>
2024-03-01 02:43:21 +0400

    added

---
+++ task13/main.js
@@ -0,0 +1,66 @@
+
+
+
+//task4
+// let array = [1, 6, 34, 5, 1, -4, 54, 76, 23, 65, 3, 50, 45, 100, 2346, -52, 6545, 0, 45, 22];
+// array.sort((a,b) => a - b)
+// console.log(array);
+
+
+
+// let person = {
+//     name: "Xanim",
+//     gender: "Qadin",
+//     birthday: "2004",
+//     age: 19,
+//     setAge: (newAge) => {
+//         person.age = newAge
+//     },
+//     getYearsBeforeRetirement() {
+//         return 65 - person.age
+//     }
+// }
+// // console.log(birthday);
+
+// // delete person.birthday
+
+
+
+// console.log(person.getYearsBeforeRetirement());
+
+//task4
+// let client = {
+//     fullname: "Xanim",
+//     creditLimit: "10000",
+//     balance: "100",
+//     precentOfMinPayment: "5",
+//     addedmoney: "100",
+//     getBalance() {
+//         if (client.balance < 0) {
+//             // alert("Balansiniz menfidir");
+//             // console.log("Balansiniz menfidir");
+//             console.log(`Borcunuz ${client.balance}`);
+//         }
+//         else {
+//             console.log(`Balansiniz ${client.balance}`);
+//         }
+//     },
+//      refill() {
+//        return client.balance + client.addedmoney
+//      }
+// }
+// console.log(client.getBalance());
+// console.log(client.refill());
+
+
+
+let country = ["Azerbaijan", "Georgia", "Turkiye"]
+country.forEach((item, index) => {
+    console.log(item.);
+}
+
+)
+const result = country.split(3)
+console.log(result);
+const uppercase = result.toUpperCase()
+for(i < 0; )
---
+++ task14/main.js
@@ -0,0 +1,29 @@
+// console.log(document.querySelector("li"));
+
+// const element = document.querySelector("h1");
+// element.remove();
+// const elem = document.createElement("li");
+// elem.textContent = "Titan";
+// document.querySelector("ul").append(elem);   
+
+//task3
+// const element = document.querySelector("h1");
+// element.textContent = "GeForce 20 Series"
+// console.log(element);
+
+// document.querySelectorAll(".primary")[0].style.background = "#ebebeb"
+// document.querySelectorAll(".primary")[1].style.background = "#ebebeb"
+
+
+//task4
+// const element = document.querySelectorAll(".primary")
+// console.log(element);
+
+//task5
+
+// if (li.className !== primary ) {
+//     li.classList.add("secondary")
+// } 
+
+
+
